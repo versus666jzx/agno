7d4b6d9446365c4f00dc3ea7e735bb88fa2f0aae
HardWorkingStation <changeme@mail.com>
2025-05-14 14:21:55 +0300

    Add support for huggingface_hub > 0.31.2

--- libs/agno/agno/embedder/huggingface.py
+++ libs/agno/agno/embedder/huggingface.py
@@ -1,13 +1,14 @@
 import json
 from dataclasses import dataclass
 from os import getenv
+from packaging import version
 from typing import Any, Dict, List, Optional, Tuple
 
 from agno.embedder.base import Embedder
 from agno.utils.log import logger
 
 try:
-    from huggingface_hub import InferenceClient
+    from huggingface_hub import InferenceClient, __version__
 except ImportError:
     logger.error("`huggingface-hub` not installed, please run `pip install huggingface-hub`")
     raise
@@ -35,18 +36,28 @@ class HuggingfaceCustomEmbedder(Embedder):
         self.huggingface_client = InferenceClient(**_client_params)
         return self.huggingface_client
 
-    def _response(self, text: str):
+    def _response_v1(self, text: str):
         return self.client.post(json={"inputs": text}, model=self.id, task=self.task)
 
-    def get_embedding(self, text: str) -> List[float]:
-        response = self._response(text=text)
-        try:
-            decoded_string = response.decode("utf-8")
-            return json.loads(decoded_string)
+    def _response_v2(self, text: str):
+        return self.client.feature_extraction(text=text, model=self.id)
 
-        except Exception as e:
-            logger.warning(e)
-            return []
+    def get_embedding(self, text: str) -> List[float]:
+        if version.parse(__version__) <= version.parse("0.31.2"):
+            response = self._response_v1(text=text)
+            try:
+                decoded_string = response.decode("utf-8")
+                return json.loads(decoded_string)
+
+            except Exception as e:
+                logger.warning(e)
+                return []
+        else:  # version huggingface_hub > 0.31.2
+            try:
+                return self._response_v2(text=text)
+            except Exception as e:
+                logger.warning(e)
+                return []
 
     def get_embedding_and_usage(self, text: str) -> Tuple[List[float], Optional[Dict]]:
         return self.get_embedding(text=text), None
